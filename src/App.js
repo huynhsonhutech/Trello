ea4d19bb0c9664d7c02dde3e03e09b578b074b12
huynhsonhutech <ronaldo211234@gmail.com>
2022-04-10 02:38:44 +0700

    Init Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className='hello'>
+          Hello World
+        </h1>
+       
+    </div>
+  );
+}
+
+export default App;
