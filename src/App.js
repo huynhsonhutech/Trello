e57cb982d97d66ce57b221fb12bae6b70acd1b69
huynhsonhutech <ronaldo211234@gmail.com>
2022-04-11 01:32:45 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,13 +1,81 @@
-import './App.css';
+import './App.scss';
 
 
 function App() {
   return (
-    <div className="App">
-        <h1 className='hello'>
-          Hello World
-        </h1>
-       
+    <div className="clone-trello-container">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className='board-column'>
+          <header>BrainStorm</header>
+            <ul>
+              <li>
+                <img 
+                  src='https://img.vn/uploads/version/img24-png-20190726133727cbvncjKzsQ.png' alt='clone-trello-alt-img'/>
+                  Title: SonHuynh
+              </li>
+              <li>Hello Brotherrrrrrrrrrrrrr</li>
+              <li>Hello Brotherrrrrrrrrrrrrr</li>
+              <li>Hello Brotherrrrrrrrrrrrrr</li>
+              <li>Hello Brotherrrrrrrrrrrrrr</li>
+            </ul>      
+          <footer>
+            Add another Card
+          </footer>
+        </div>
+        <div className='board-column'>
+          <header>BrainStorm</header>
+            <ul>
+              <li>
+                <img 
+                  src='https://img.vn/uploads/version/img24-png-20190726133727cbvncjKzsQ.png' alt='clone-trello-alt-img'/>
+                  Title: SonHuynh
+              </li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+            </ul>      
+          <footer>
+            Add another Card
+          </footer>
+        </div>
+        <div className='board-column'>
+          <header>BrainStorm</header>
+            <ul>
+              <li>
+                <img 
+                  src='https://img.vn/uploads/version/img24-png-20190726133727cbvncjKzsQ.png' alt='clone-trello-alt-img'/>
+                  Title: SonHuynh
+              </li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+            </ul>      
+          <footer>
+            Add another Card
+          </footer>
+        </div>
+        <div className='board-column'>
+          <header>BrainStorm</header>
+            <ul>
+              <li>
+                <img 
+                  src='https://img.vn/uploads/version/img24-png-20190726133727cbvncjKzsQ.png' alt='clone-trello-alt-img'/>
+                  Title: SonHuynh
+              </li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+              <li>Hello Brother</li>
+            </ul>      
+          <footer>
+            Add another Card
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
